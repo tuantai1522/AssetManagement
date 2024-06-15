ff27b8192d058b6fd73c79a65e1aacdfbf1a552e
Thuan <thuanbui2611@gmail.com>
2024-06-15 13:21:11 +0700

    frontend: setup source code structure, related package.

---
+++ AssetManagement.Application/FrontEnd/src/app/layout/DefaultLayout.tsx
@@ -0,0 +1,10 @@
+import { Outlet } from "react-router-dom";
+
+const DefaultLayout = () => {
+    return(
+        <div>
+            <Outlet />
+        </div>
+    )
+}
+export default DefaultLayout;
---
+++ AssetManagement.Application/FrontEnd/src/app/routes/router.tsx
@@ -0,0 +1,25 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import App from "../../App";
+import DefaultLayout from "../layout/DefaultLayout";
+import HomePage from "../../pages/home";
+import NotFound from "../errors/NotFound";
+import ServerErrors from "../errors/ServerErrors";
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                element: <DefaultLayout/>,
+                children: [
+                    { path: "", element: <HomePage/> },
+                ]
+            }
+        ]
+    },
+    
+    { path: "not-found", element: <NotFound /> },
+    { path: "server-error", element: <ServerErrors /> },
+    { path: "*", element: <Navigate replace to="/not-found" /> },
+]);
