acc1727b65c04d9450d39af1625d8844a47a8964
Bui Cong Thuan <thuanbui2611@gmail.com>
2024-06-18 17:46:18 +0700

    create AppTextInput components

---
+++ AssetManagement.Application/FrontEnd/src/app/components/AppTextInput.tsx
@@ -0,0 +1,48 @@
+import { TextField } from "@mui/material";
+import { useController, UseControllerProps } from "react-hook-form";
+
+interface Props extends UseControllerProps {
+  label?: string;
+  placeholder?: string;
+  size?: "small" | "medium";
+  multiline?: boolean;
+  rows?: number;
+  type?: string;
+  error?: boolean;
+  helperText?: string;
+  className?: string;
+  InputProps?: any | undefined;
+  defaultValue?: string | "";
+  value?: any | undefined;
+  maxRows?: number;
+  sx?: any;
+  disabled?: boolean;
+  onKeyDown?: any;
+}
+
+export default function AppTextInput(props: Props) {
+  const { fieldState, field } = useController({ ...props, defaultValue: "" });
+
+  return (
+    <TextField
+      {...props}
+      {...field}
+      label={props.label}
+      sx={props.sx ? props.sx : ""}
+      multiline={props.multiline}
+      rows={props.rows}
+      maxRows={props.maxRows}
+      type={props.type}
+      size={props.size ?? "small"}
+      placeholder={props.placeholder}
+      disabled={props.disabled}
+      variant="outlined"
+      onKeyDown={props.onKeyDown}
+      value={props.value}
+      className={props.className}
+      error={props.error || !!fieldState.error}
+      helperText={props.error ? props.helperText : fieldState.error?.message}
+      {...(props.InputProps && `inputProps=${props.InputProps}`)}
+    />
+  );
+}
