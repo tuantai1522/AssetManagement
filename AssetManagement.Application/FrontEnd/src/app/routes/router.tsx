83790cf39db242989d0b1f5d7118457bd1164f84
Bui Cong Thuan <thuanbui2611@gmail.com>
2024-06-19 11:24:52 +0700

    [FE] Add Authentication/Authorization and pagination

--- AssetManagement.Application/FrontEnd/src/app/routes/router.tsx
+++ AssetManagement.Application/FrontEnd/src/app/routes/router.tsx
@@ -7,19 +7,20 @@ import ServerErrors from "../errors/ServerErrors";
 import LoginPage from "../../pages/authentication/LoginPage";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        element: <DefaultLayout />,
-        children: [{ path: "", element: <HomePage /> }],
-      },
-    ],
-  },
-  { path: "login", element: <LoginPage /> },
-
-  { path: "not-found", element: <NotFound /> },
-  { path: "server-error", element: <ServerErrors /> },
-  { path: "*", element: <Navigate replace to="/not-found" /> },
+    {
+        path: "/",
+        element: <App/>,
+        children: [
+            {
+                element: <DefaultLayout/>,
+                children: [
+                    { path: "", element: <HomePage/> },
+                    { path: "user-manage", element: <ManagementUserPage/> },
+                ]
+            }
+        ]
+    },
+    { path: "not-found", element: <NotFound /> },
+    { path: "server-error", element: <ServerErrors /> },
+    { path: "*", element: <Navigate replace to="/not-found" /> }
 ]);
