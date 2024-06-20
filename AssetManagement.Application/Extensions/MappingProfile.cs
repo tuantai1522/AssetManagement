11c0684986b50455fe5188c48e961283721592ea
Dat Bui <bvdat1110@outlook.com.vn>
2024-06-20 13:28:38 +0700

    Delete redundant setup in Program

--- AssetManagement.Application/Extensions/MappingProfile.cs
+++ AssetManagement.Application/Extensions/MappingProfile.cs
@@ -9,8 +9,10 @@ public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+        //User
         CreateMap<AppUser, UserInfoResponse>()
             .ForMember(dest => dest.FullName, src => src.MapFrom(src => string.Concat(src.LastName, " ", src.FirstName)))
             .ForMember(dest => dest.Types, src => src.MapFrom(src => src.UserRoles.Select(x => x.Role.Name)));
+        //Assert
     }
 }
