83dbd93826bfaa6580d9a4236c9438143bf9148b
Bui Cong Thuan <thuanbui2611@gmail.com>
2024-06-21 23:32:41 +0700

    update Spa RootPath

--- AssetManagement.Application/Program.cs
+++ AssetManagement.Application/Program.cs
@@ -24,7 +24,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSpaStaticFiles(configuration =>
 {
-    configuration.RootPath = "Frontend/build";
+    configuration.RootPath = "Frontend";
 });
 
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
