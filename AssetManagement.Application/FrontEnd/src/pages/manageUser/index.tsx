ac5cdcaaac08cb7d5b936ac8e8177505d9791d8e
congbinh75 <dangcongbinh36@gmail.com>
2024-06-21 18:09:51 +0700

    Integrate disabling button and logic to UserList

--- AssetManagement.Application/FrontEnd/src/pages/manageUser/index.tsx
+++ AssetManagement.Application/FrontEnd/src/pages/manageUser/index.tsx
@@ -5,34 +5,41 @@ import ConfirmModal from "../../app/components/confirmModal";
 import UserList from "./userList/userList";
 
 export default function ManagementUserPage() {
-
   // const [users, setUsers] = useState<FilterUser[]>([]);
-  const { data, isLoading, error } = agent.Users.filter();
+  const { data, isLoading, error, mutate } = agent.Users.filter();
   console.log(`Data: ${JSON.stringify(data, null, 2)}`);
 
-  const [isDisablingModalOpen, setDisablingModalOpen] = useState(false);
+  const [isDisablingModalOpen, setIsDisablingModalOpen] = useState(false);
   const [currentDisablingId, setCurrentDisablingId] = useState("");
 
   const handleDisable = (id: string) => {
-    if (!id && id !== "") agent.Users.disable(id);
+    agent.Users.disable(id);
+    mutate();
   };
-
 
   return (
     <div className="flex justify-center h-full">
       <div className="container">
-        <p className="text-primary text-xl font-bold justify-start items-start">User List</p>
+        <p className="text-primary text-xl font-bold justify-start items-start">
+          User List
+        </p>
         <div className="mt-3">
-          <UserList data={data?.items?.result} error={error} isLoading={isLoading} />
+          <UserList
+            data={data?.items?.result}
+            error={error}
+            isLoading={isLoading}
+            setIsOpenDisablingModal={setIsDisablingModalOpen}
+            setCurrentDisablingId={setCurrentDisablingId}
+          />
         </div>
       </div>
       <ConfirmModal
         message="Do you want to disable this user?"
         confirmMessage="Disable"
         isOpen={isDisablingModalOpen}
-        onClose={() => setDisablingModalOpen(false)}
+        onClose={() => setIsDisablingModalOpen(false)}
         onConfirm={() => {
-          setDisablingModalOpen(false);
+          setIsDisablingModalOpen(false);
           handleDisable(currentDisablingId);
         }}
       />
@@ -43,6 +50,5 @@ export default function ManagementUserPage() {
         onClose={() => {}}
       />  */}
     </div>
-
   );
 }
