bb0c641f5cd56adaf437bf7ac06ad0f3b0d5c456
Blue Teeth <daominhtri1000@gmail.com>
2024-06-23 15:33:44 +0700

    Update search user feature

--- AssetManagement.Application/Frontend/src/pages/manageUser/index.tsx
+++ AssetManagement.Application/Frontend/src/pages/manageUser/index.tsx
@@ -5,7 +5,8 @@ import { Order } from "../../app/components/table/sortTable";
 import UserList from "./userList/userList";
 import { convertUtcToLocalDate } from "../../app/utils/dateUtils";
 import { FilterUser } from "../../app/models/User";
-import { Pagination, Stack } from "@mui/material";
+import { Pagination, Stack, TextField } from "@mui/material";
+import AppTextInput from "../../app/components/AppTextInput";
 
 type OrderByFieldName = "staffCode" | "fullName" | "joinedDate" | "type" | "lastUpdate";
 
@@ -17,6 +18,8 @@ export default function ManagementUserPage() {
     pageSize: 5
   });
 
+  const [searchInput, setSearchInput] = useState<string>("");
+
   const [order, setOrder] = useState<Order>("desc");
   const [orderBy, setOrderBy] = useState<OrderByFieldName>("joinedDate");
 
@@ -27,31 +30,31 @@ export default function ManagementUserPage() {
       case "staffCode":
         {
           setQuery((query) => ({ ...query, sortStaffCode: order, sortFullName: undefined, sortJoinedDate: undefined, sortType: undefined, sortLastUpdate: undefined }));
-          mutate();
+          mutate(query);
           break;
         }
       case "fullName":
         {
           setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: order, sortJoinedDate: undefined, sortType: undefined, sortLastUpdate: undefined }));
-          mutate();
+          mutate(query);
           break;
         }
       case "joinedDate":
         {
           setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: undefined, sortJoinedDate: order, sortType: undefined, sortLastUpdate: undefined }));
-          mutate(); 
+          mutate(query);
           break;
         }
       case "type":
         {
           setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: undefined, sortJoinedDate: undefined, sortType: order, sortLastUpdate: undefined }));
-          mutate();
+          mutate(query);
           break;
         }
-        case "lastUpdate":
+      case "lastUpdate":
         {
           setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: undefined, sortJoinedDate: undefined, sortType: undefined, sortLastUpdate: order }));
-          mutate();
+          mutate(query);
           break;
         }
       default:
@@ -60,21 +63,36 @@ export default function ManagementUserPage() {
 
   }, [orderBy, order])
 
-  useEffect(() => {
-    const reloadData = async () => {
-      await mutate(query);
-    };
+  // useEffect(() => {
+  //   const reloadData = async () => {
+  //     await mutate(query);
+  //   };
 
-    reloadData();
+  //   reloadData();
 
-    return () => {
-    };
-  }, [query.pageNumber]);
+  //   return () => {
+  //   };
+  // }, [query.pageNumber]);
 
   const handlePageNumberChange = (value: any) => {
     const pageNumber = Number(value);
     setQuery((prevQuery) => ({ ...prevQuery, pageNumber }));
+    mutate(query);
   };
+
+  const handleQueryInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setSearchInput(value);
+  }
+
+  const handleSearchClick = () => {
+    setQuery((prevQuery) => ({
+      ...prevQuery,
+      name: searchInput,
+      pageNumber: 1,
+    }));
+    mutate(query);
+  }
 
   console.log(`Data: `, data);
 
@@ -82,6 +100,25 @@ export default function ManagementUserPage() {
     <div className="flex justify-center h-full">
       <div className="container">
         <p className="text-primary text-xl font-bold justify-start items-start">User List</p>
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          alignItems="center"
+          spacing={2}
+          className="mt-3"
+        >
+          <Stack
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="center"
+            spacing={2}
+          >
+            <input type="text" placeholder="Search" name="name" value={searchInput} onChange={handleQueryInputChange} />
+            <button onClick={handleSearchClick}>Search</button>
+
+          </Stack>
+
+        </Stack>
         <div className="mt-3">
           <UserList
             data={data?.items?.result?.map((item: FilterUser) => ({
