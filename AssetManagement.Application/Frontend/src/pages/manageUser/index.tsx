aa7265d57a78ec60165f7e62e8c88e1f5af135d9
congbinh75 <dangcongbinh36@gmail.com>
2024-06-24 14:58:35 +0700

    Correct merge mistakes

--- AssetManagement.Application/Frontend/src/pages/manageUser/index.tsx
+++ AssetManagement.Application/Frontend/src/pages/manageUser/index.tsx
@@ -11,16 +11,21 @@ import UsePagination from "../../app/components/paginationButtons/paginationButt
 import { Search } from "@mui/icons-material";
 import ConfirmModal from "../../app/components/confirmModal";
 
-type OrderByFieldName = "staffCode" | "fullName" | "joinedDate" | "type" | "lastUpdate";
+type OrderByFieldName =
+  | "staffCode"
+  | "fullName"
+  | "joinedDate"
+  | "type"
+  | "lastUpdate";
 
 export default function ManagementUserPage() {
-  const [clickOnUser, setClickOnUser] = useState<boolean>(false)
-  const [userId, setUserId] = useState<string>("0")
+  const [clickOnUser, setClickOnUser] = useState<boolean>(false);
+  const [userId, setUserId] = useState<string>("0");
 
   const [query, setQuery] = useState<UserQuery>({
     sortJoinedDate: "desc",
     pageNumber: 1,
-    pageSize: 5
+    pageSize: 5,
   });
   const [isDisablingModalOpen, setIsDisablingModalOpen] = useState(false);
   const [currentDisablingId, setCurrentDisablingId] = useState("");
@@ -31,59 +36,96 @@ export default function ManagementUserPage() {
   const [orderBy, setOrderBy] = useState<OrderByFieldName>("joinedDate");
 
   const { data, isLoading, error, mutate } = agent.Users.filter(query);
-  const { data: userData, isLoading: userLoading, error: userError } = agent.Users.details(userId);
+  const {
+    data: userData,
+    isLoading: userLoading,
+    error: userError,
+  } = agent.Users.details(userId);
 
   useEffect(() => {
     switch (orderBy) {
-      case "staffCode":
-        {
-          setQuery((query) => ({ ...query, sortStaffCode: order, sortFullName: undefined, sortJoinedDate: undefined, sortType: undefined, sortLastUpdate: undefined }));
-          mutate(query);
-          break;
-        }
-      case "fullName":
-        {
-          setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: order, sortJoinedDate: undefined, sortType: undefined, sortLastUpdate: undefined }));
-          mutate(query);
-          break;
-        }
-      case "joinedDate":
-        {
-          setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: undefined, sortJoinedDate: order, sortType: undefined, sortLastUpdate: undefined }));
-          mutate(query);
-          break;
-        }
-      case "type":
-        {
-          setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: undefined, sortJoinedDate: undefined, sortType: order, sortLastUpdate: undefined }));
-          mutate(query);
-          break;
-        }
-      case "lastUpdate":
-        {
-          setQuery((query) => ({ ...query, sortStaffCode: undefined, sortFullName: undefined, sortJoinedDate: undefined, sortType: undefined, sortLastUpdate: order }));
-          mutate(query);
-          break;
-        }
+      case "staffCode": {
+        setQuery((query) => ({
+          ...query,
+          sortStaffCode: order,
+          sortFullName: undefined,
+          sortJoinedDate: undefined,
+          sortType: undefined,
+          sortLastUpdate: undefined,
+        }));
+        mutate(query);
+        break;
+      }
+      case "fullName": {
+        setQuery((query) => ({
+          ...query,
+          sortStaffCode: undefined,
+          sortFullName: order,
+          sortJoinedDate: undefined,
+          sortType: undefined,
+          sortLastUpdate: undefined,
+        }));
+        mutate(query);
+        break;
+      }
+      case "joinedDate": {
+        setQuery((query) => ({
+          ...query,
+          sortStaffCode: undefined,
+          sortFullName: undefined,
+          sortJoinedDate: order,
+          sortType: undefined,
+          sortLastUpdate: undefined,
+        }));
+        mutate(query);
+        break;
+      }
+      case "type": {
+        setQuery((query) => ({
+          ...query,
+          sortStaffCode: undefined,
+          sortFullName: undefined,
+          sortJoinedDate: undefined,
+          sortType: order,
+          sortLastUpdate: undefined,
+        }));
+        mutate(query);
+        break;
+      }
+      case "lastUpdate": {
+        setQuery((query) => ({
+          ...query,
+          sortStaffCode: undefined,
+          sortFullName: undefined,
+          sortJoinedDate: undefined,
+          sortType: undefined,
+          sortLastUpdate: order,
+        }));
+        mutate(query);
+        break;
+      }
       default:
         break;
     }
-
-  }, [orderBy, order])
+  }, [orderBy, order]);
 
   const handlePageNumberChange = (value: any) => {
     const pageNumber = Number(value);
     setQuery((prevQuery) => ({ ...prevQuery, pageNumber }));
     mutate(query);
+  };
+
   const handleDisable = (id: string) => {
     agent.Users.disable(id);
     mutate();
   };
 
-  const handleQueryInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleQueryInputChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
     const { name, value } = event.target;
     setSearchInput(value);
-  }
+  };
 
   const handleSerchSubmit = () => {
     setQuery((prevQuery) => ({
@@ -92,17 +134,18 @@ export default function ManagementUserPage() {
       pageNumber: 1,
     }));
     mutate(query);
-  }
+  };
 
-  console.log(`Data: `, data);
   const handleClickOnUser = (rowId: string) => {
     setClickOnUser(true);
     setUserId(data.items.result[rowId].id);
-  }
+  };
   return (
     <div className="flex justify-center h-full">
       <div className="container">
-        <p className="text-primary text-xl font-bold justify-start items-start">User List</p>
+        <p className="text-primary text-xl font-bold justify-start items-start">
+          User List
+        </p>
         <Stack
           direction="row"
           justifyContent="space-between"
@@ -116,13 +159,21 @@ export default function ManagementUserPage() {
             alignItems="center"
             spacing={2}
           >
-            <input type="text" placeholder="Search" name="name" value={searchInput} onChange={handleQueryInputChange} />
-            <IconButton aria-label="Search" onClick={handleSerchSubmit} size="small">
+            <input
+              type="text"
+              placeholder="Search"
+              name="name"
+              value={searchInput}
+              onChange={handleQueryInputChange}
+            />
+            <IconButton
+              aria-label="Search"
+              onClick={handleSerchSubmit}
+              size="small"
+            >
               <Search />
             </IconButton>
-
           </Stack>
-
         </Stack>
         <div className="mt-3">
           <UserList
@@ -137,6 +188,8 @@ export default function ManagementUserPage() {
             orderBy={orderBy}
             setOrderBy={setOrderBy}
             handleClick={(event, rowId) => handleClickOnUser(rowId)}
+            setIsOpenDisablingModal={setIsDisablingModalOpen}
+            setCurrentDisablingId={setCurrentDisablingId}
           />
 
           <Stack
@@ -144,7 +197,10 @@ export default function ManagementUserPage() {
             justifyContent="flex-end"
             alignItems="baseline"
           >
-            <UsePagination totalPage={data?.metaData?.totalPageCount ?? 1} onChange={handlePageNumberChange} />
+            <UsePagination
+              totalPage={data?.metaData?.totalPageCount ?? 1}
+              onChange={handlePageNumberChange}
+            />
           </Stack>
         </div>
       </div>
@@ -154,6 +210,8 @@ export default function ManagementUserPage() {
         userData={userData?.result}
         onClose={() => {
           setClickOnUser(false);
+        }}
+      ></UserInfo>
       <ConfirmModal
         message="Do you want to disable this user?"
         confirmMessage="Disable"
@@ -163,9 +221,7 @@ export default function ManagementUserPage() {
           setIsDisablingModalOpen(false);
           handleDisable(currentDisablingId);
         }}
-      ></UserInfo>
+      />
     </div>
   );
 }
-
-
--- AssetManagement.Application/Frontend/src/pages/manageUser/userList/userList.tsx
+++ AssetManagement.Application/Frontend/src/pages/manageUser/userList/userList.tsx
@@ -1,90 +1,97 @@
-import { useState } from "react";
-import AppTable, { ColumnDefinition, Order } from "../../../app/components/table/sortTable";
+import { Dispatch, SetStateAction, useState } from "react";
+import AppTable, {
+  ColumnDefinition,
+  Order,
+} from "../../../app/components/table/sortTable";
 import { FilterUser } from "../../../app/models/User";
 import { Button } from "@mui/material";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
+import EditIcon from '@mui/icons-material/Edit';
+
 export interface UserListProp {
-    data: FilterUser[],
-    isLoading: boolean,
-    error: any,
-    order: Order,
-    setOrder: (order: Order) => void,
-    orderBy: any,
-    setOrderBy: (orderBy: any) => void,
-    handleClick: (event:any, rowId: string) => void,
+  data: FilterUser[];
+  isLoading: boolean;
+  error: any;
+  order: Order;
+  setOrder: (order: Order) => void;
+  orderBy: any;
+  setOrderBy: (orderBy: any) => void;
+  handleClick: (event: any, rowId: string) => void;
+  setIsOpenDisablingModal: Dispatch<SetStateAction<boolean>>;
+  setCurrentDisablingId: Dispatch<SetStateAction<string>>;
 }
 
+const rows: FilterUser[] = [];
+
 export default function UserList(props: UserListProp) {
-const columns: ColumnDefinition[] = [
+  const columns: ColumnDefinition[] = [
     {
-        id: 'staffCode',
-        fieldName: "staffCode",
-        disablePadding: true,
-        label: 'Staff Code',
-        className: "font-bold",
-        rowRatio: "w-1/12",
-        style: {
-            border: "none",
-            borderBottom: "none",
-            minWidth: "100px"
-        }
-    },
-    },
-    {
-        id: 'fullName',
-        fieldName: 'fullName',
-        disablePadding: false,
-        label: 'Full Name',
-        className: "font-bold",
-        style: {
-            border: "none",
-            borderBottom: "none",
-            minWidth: "250px"
-        },
-
-        rowRatio: "w-4/12",
-        // renderCell: <button color="primary" className="active:bg-black" onClick={e => {
-        //     e.stopPropagation();
-        //     alert("child");
-        // }
-        // } > Test</button>
-
+      id: "staffCode",
+      fieldName: "staffCode",
+      disablePadding: true,
+      label: "Staff Code",
+      className: "font-bold",
+      rowRatio: "w-1/12",
+      style: {
+        border: "none",
+        borderBottom: "none",
+        minWidth: "100px",
+      },
     },
     {
-        id: 'username',
-        fieldName: 'username',
-        disablePadding: false,
-        label: 'Username',
-        className: "font-bold ",
-        style: {
-            border: "none",
-            borderBottom: "none"
-        },
-        rowRatio: "w-2/12",
-        disableSort: true,
+      id: "fullName",
+      fieldName: "fullName",
+      disablePadding: false,
+      label: "Full Name",
+      className: "font-bold",
+      style: {
+        border: "none",
+        borderBottom: "none",
+        minWidth: "250px",
+      },
+  
+      rowRatio: "w-4/12",
+      // renderCell: <button color="primary" className="active:bg-black" onClick={e => {
+      //     e.stopPropagation();
+      //     alert("child");
+      // }
+      // } > Test</button>
     },
     {
-        id: 'joinedDate',
-        fieldName: 'joinedDate',
-        disablePadding: false,
-        label: 'Joined Date',
-        className: "font-bold ",
-        style: {
-            border: "none",
-            borderBottom: "none",
-            minWidth: "120px"
-        },
-        rowRatio: "w-2/12",
+      id: "username",
+      fieldName: "username",
+      disablePadding: false,
+      label: "Username",
+      className: "font-bold ",
+      style: {
+        border: "none",
+        borderBottom: "none",
+      },
+      rowRatio: "w-2/12",
+      disableSort: true,
     },
     {
-        id: 'type',
-        fieldName: 'types',
-        disablePadding: false,
-        label: 'Type',
-        className: "font-bold ",
-        style: {
-            border: "none",
-            borderBottom: "none",
-            minWidth: "80px",
+      id: "joinedDate",
+      fieldName: "joinedDate",
+      disablePadding: false,
+      label: "Joined Date",
+      className: "font-bold ",
+      style: {
+        border: "none",
+        borderBottom: "none",
+        minWidth: "120px",
+      },
+      rowRatio: "w-2/12",
+    },
+    {
+      id: "type",
+      fieldName: "types",
+      disablePadding: false,
+      label: "Type",
+      className: "font-bold ",
+      style: {
+        border: "none",
+        borderBottom: "none",
         minWidth: "100px",
       },
       rowRatio: "w-2/12",
@@ -97,8 +104,8 @@ const columns: ColumnDefinition[] = [
       className: "font-bold",
       bodyStyle: {
         borderBottom: "none",
-        },
-        rowRatio: "w-1/12",
+      },
+      rowRatio: "w-1/12",
       renderCell: (params) => (
         <div className="ml-2 flex w-fit justify-end">
           <button
@@ -128,24 +135,18 @@ const columns: ColumnDefinition[] = [
       ),
     },
   ];
-
-];
-
-const rows: FilterUser[] = []
-
-export default function UserList(props: UserListProp) {
-
-    return (
-        <div className="min-h-60">
-            <AppTable<FilterUser>
-                order={props.order}
-                setOrder={props.setOrder}
-                orderByFieldName={props.orderBy}
-                setOrderByFieldName={props.setOrderBy}
-                columns={columns}
-                rows={props.data}
-                handleClick={props.handleClick}
-            />
-        </div>
-    );
+  
+  return (
+    <div className="min-h-60">
+      <AppTable<FilterUser>
+        order={props.order}
+        setOrder={props.setOrder}
+        orderByFieldName={props.orderBy}
+        setOrderByFieldName={props.setOrderBy}
+        columns={columns}
+        rows={props.data}
+        handleClick={props.handleClick}
+      />
+    </div>
+  );
 }
