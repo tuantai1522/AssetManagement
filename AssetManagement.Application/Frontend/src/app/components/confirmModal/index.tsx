7e0ba76ed379900210e4bd02714662a26e3a006d
congbinh75 <dangcongbinh36@gmail.com>
2024-06-25 08:58:42 +0700

    Update ConfirmModal UI

--- AssetManagement.Application/Frontend/src/app/components/confirmModal/index.tsx
+++ AssetManagement.Application/Frontend/src/app/components/confirmModal/index.tsx
@@ -24,7 +24,7 @@ const ConfirmModal = ({
   return (
     <>
       <div className="fixed w-screen h-screen bg-gray-400 top-0 left-0 opacity-50"></div>
-      <div id="confirmModal" className="z-10 shadow-2xl absolute">
+      <div id="confirmModal" className="z-10 shadow-2xl absolute top-1/2 -translate-y-1/2">
         <div className="bg-white rounded-lg border border-black w-[32rem]">
           <div className="bg-slate-100 rounded-t-lg border-b border-black px-12 py-5">
             <h2 className="text-lg font-bold text-primary">{titleMessage}</h2>
