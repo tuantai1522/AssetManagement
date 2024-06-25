a2487903b27e0de503cb81c7f3b9dce6ba633daf
congbinh75 <dangcongbinh36@gmail.com>
2024-06-25 17:08:34 +0700

    Merge "dev" into "features/disable-user"

---
+++ AssetManagement.Data/Data/MappingConfigurations/AssetConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using AssetManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AssetManagement.Data.Data.MappingConfigurations
+{
+    internal class AssetConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Asset> builder)
+        {
+            #region define index
+
+            builder.HasOne(asset => asset.Category).WithMany(cat => cat.Assets).HasForeignKey(asset => asset.CategoryId).IsRequired();
+
+            #endregion
+        }
+    }
+}
