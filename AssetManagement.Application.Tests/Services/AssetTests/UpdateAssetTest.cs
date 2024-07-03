3a4e8015d0262d61fcb2df9db953a7fd74db05a0
tai <tai.bang157@gmail.com>
2024-07-03 11:24:04 +0700

    [BE] - Write Unit test to update Asset

--- AssetManagement.Application.Tests/Services/AssetTests/UpdateAssetTest.cs
+++ AssetManagement.Application.Tests/Services/AssetTests/UpdateAssetTest.cs
@@ -1,43 +1,63 @@
-using AssetManagement.Contracts.Dtos.AssetDtos.Requests;
+﻿using AssetManagement.Contracts.Dtos.AssetDtos.Requests;
 using AssetManagement.Domain.Entities;
+using AssetManagement.Domain.Enums;
 using AssetManagement.Domain.Exceptions;
 using AutoFixture;
 using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AssetManagement.Application.Tests.Services.AssetTests
 {
-    public class GetAssetByIdTest : SetupAssetServiceTest
+    public class UpdateAssetTest : SetupAssetServiceTest
     {
-        public GetAssetByIdTest()
+        public UpdateAssetTest()
         {
 
         }
 
         [Fact]
-        public async Task GetAssetById_IdIsEmpty_ThrowsBadRequestException()
+        public async Task UpdateAssetById_IdIsEmpty_ThrowsBadRequestException()
         {
             // Arrange
-            var request = new AssetDetailsRequest
+            var request = new AssetUpdateRequest
             {
-                Id = Guid.Empty
+                AssetId = Guid.Empty
             };
 
             // Act & Assert
             var exception = await Assert.ThrowsAsync<BadRequestException>
-                (() => _assetService.GetAssetByIdAsync(request));
+                (() => _assetService.UpdateAssetAsync(request));
 
-            Assert.Equal("Please provide id to get asset", exception.Message);
+            Assert.Equal("Please provide id to update asset", exception.Message);
         }
 
         [Fact]
-        public async Task GetAssetById_CanNotFindAsset_ThrowsNotFoundException()
+        public async Task UpdateAssetById_AnyFieldIsEmpty_ThrowsBadRequestException()
         {
             // Arrange
-            var request = new AssetDetailsRequest
+            var request = new AssetUpdateRequest
             {
-                Id = Guid.NewGuid()
+                AssetId = Guid.NewGuid(),
+                AssetName = "",
             };
+
+            // Act & Assert
+            var exception = await Assert.ThrowsAsync<BadRequestException>
+                (() => _assetService.UpdateAssetAsync(request));
+
+            Assert.Equal("Please provide full info to update asset", exception.Message);
+        }
+
+        [Fact]
+        public async Task UpdateAssetById_CanNotFindAsset_ThrowsNotFoundException()
+        {
+            // Arrange
+            var request = _fixture.Create<AssetUpdateRequest>();
 
             _unitOfWorkMock.Setup(x => x.AssetRepository.Get(
                                     It.IsAny<Expression<Func<Asset, bool>>>(),
@@ -53,18 +73,16 @@ namespace AssetManagement.Application.Tests.Services.AssetTests
 
 
             // Act & Assert
-            var exception = await Assert.ThrowsAsync<NotFoundException>(() => _assetService.GetAssetByIdAsync(request));
+            var exception = await Assert.ThrowsAsync<NotFoundException>(() => _assetService.UpdateAssetAsync(request));
             Assert.Equal("Can't find asset", exception.Message);
         }
 
         [Fact]
-        public async Task GetAssetById_CanNotFindUser_ThrowsNotFoundException()
+        public async Task UpdateAssetById_CanNotFindUser_ThrowsNotFoundException()
         {
             // Arrange
-            var request = new AssetDetailsRequest
-            {
-                Id = Guid.NewGuid()
-            };
+            var request = _fixture.Create<AssetUpdateRequest>();
+
             // Mock AssetRepo to return null for any FindByIdAsync call
             List<Asset> Assets = new List<Asset>
                 {
@@ -90,25 +108,21 @@ namespace AssetManagement.Application.Tests.Services.AssetTests
                             .ReturnsAsync(null as AppUser);
 
             // Act & Assert
-            var exception = await Assert.ThrowsAsync<NotFoundException>(() => _assetService.GetAssetByIdAsync(request));
-            Assert.Equal("Can't find user", exception.Message);
+            var exception = await Assert.ThrowsAsync<NotFoundException>(() => _assetService.UpdateAssetAsync(request));
+            Assert.Equal("Can't find user to update", exception.Message);
         }
 
         [Fact]
-        public async Task GetAssetById_UserCanNotViewAsset_ThrowsUnauthorizedException()
+        public async Task UpdateAssetById_UserIsDisabledCanNotUpdate_ThrowsUnauthorizedException()
         {
             // Arrange
-            var request = new AssetDetailsRequest
-            {
-                Id = Guid.NewGuid()
-            };
+            var request = _fixture.Create<AssetUpdateRequest>();
+
             // Mock AssetRepo to return null for any FindByIdAsync call
+
             List<Asset> Assets = new List<Asset>
                 {
-                    _fixture.Build<Asset>()
-                            .With(x => x.Location, "HCM")
-                            .With(x => x.Id, request.Id)
-                            .Create(),
+                    _fixture.Create<Asset>(),
                     _fixture.Create<Asset>(),
                     _fixture.Create<Asset>(),
                 };
@@ -126,7 +140,7 @@ namespace AssetManagement.Application.Tests.Services.AssetTests
                });
 
             var user = _fixture.Build<AppUser>()
-                               .With(x => x.Location, "HN")
+                               .With(r => r.IsDisabled, true)
                                .Create();
 
             // Mock UserManager to return null for any FindByIdAsync call
@@ -134,25 +148,21 @@ namespace AssetManagement.Application.Tests.Services.AssetTests
                             .ReturnsAsync(user);
 
             // Act & Assert
-            var exception = await Assert.ThrowsAsync<UnauthorizedException>(() => _assetService.GetAssetByIdAsync(request));
-            Assert.Equal("This user can't view this asset", exception.Message);
+            var exception = await Assert.ThrowsAsync<UnauthorizedException>(() => _assetService.UpdateAssetAsync(request));
+            Assert.Equal("This user can't update this asset", exception.Message);
         }
 
-
         [Fact]
-        public async Task GetAssetById_UserCanViewAsset_ShouldReturnAssetDetailsResponse()
+        public async Task UpdateAssetById_UserCanUpdateAsset_ShouldReturnNull()
         {
             // Arrange
-            var request = new AssetDetailsRequest
-            {
-                Id = Guid.NewGuid()
-            };
+            var request = _fixture.Create<AssetUpdateRequest>();
+
             // Mock AssetRepo to return null for any FindByIdAsync call
             List<Asset> Assets = new List<Asset>
                 {
                     _fixture.Build<Asset>()
                             .With(x => x.Location, "HCM")
-                            .With(x => x.Id, request.Id)
                             .Create(),
                     _fixture.Create<Asset>(),
                     _fixture.Create<Asset>(),
@@ -171,17 +181,20 @@ namespace AssetManagement.Application.Tests.Services.AssetTests
                });
 
             var user = _fixture.Build<AppUser>()
+                               .With(r => r.IsDisabled, false)
                                .With(x => x.Location, "HCM")
                                .Create();
 
             // Mock UserManager to return null for any FindByIdAsync call
             _userManagerMock.Setup(um => um.FindByIdAsync(It.IsAny<string>()))
                             .ReturnsAsync(user);
-
+            
             // Act & Assert
-            var response = await _assetService.GetAssetByIdAsync(request);
+            await _assetService.UpdateAssetAsync(request);
 
-            Assert.NotNull(response);
+            _unitOfWorkMock.Verify(repo => repo.AssetRepository.Update(Assets[0]), Times.Once);
+            _unitOfWorkMock.Verify(repo => repo.SaveChangesAsync(), Times.Once);
         }
+
     }
 }
