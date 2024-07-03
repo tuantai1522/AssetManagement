6c990c47325ba75b7236b38156c417df273dc9a1
tai <tai.bang157@gmail.com>
2024-07-03 14:25:35 +0700

    [FE] - To add breadcrumb on header

--- AssetManagement.Application/Frontend/src/app/layout/Header.tsx
+++ AssetManagement.Application/Frontend/src/app/layout/Header.tsx
@@ -11,48 +11,50 @@ const Header = () => {
   const location = useLocation();
   const pathName = location.pathname.split("/");
   let breadcrumb = "";
-    let pageName = pathName[1];
-    switch(pageName) {
-      case "":
-        breadcrumb = "Home";
+  let pageName = pathName[1];
+  switch (pageName) {
+    case "":
+      breadcrumb = "Home";
+      break;
+    case "manage-user":
+      breadcrumb = "Manage User";
+      break;
+    case "manage-asset":
+      breadcrumb = "Manage Asset";
+      break;
+    case "manage-assignment":
+      breadcrumb = "Manage Assignment";
+      break;
+    case "request-for-returning":
+      breadcrumb = "Request for Returning";
+      break;
+    case "report":
+      breadcrumb = "Report";
+      break;
+    default:
+      breadcrumb = "";
+  }
+
+  if (pathName.length >= 3) {
+    breadcrumb += " > ";
+    let name = pathName[2];
+    switch (name) {
+      case "create-user":
+        breadcrumb += "Create New User";
         break;
-      case "manage-user":
-        breadcrumb = "Manage User";
+      case "edit-user":
+        breadcrumb += "Edit User";
         break;
-      case "manage-asset":
-        breadcrumb = "Manage Asset";
+      case "create-asset":
+        breadcrumb += "Create New Asset";
         break;
-      case "manage-assignment":
-        breadcrumb = "Manage Assignment";
-        break;
-      case "request-for-returning":
-        breadcrumb = "Request for Returning";
-        break;
-      case "report":
-        breadcrumb = "Report"
+      case "edit-asset":
+        breadcrumb += "Edit Asset";
         break;
       default:
-        breadcrumb = "";
+        breadcrumb += "";
     }
-
-    if (pathName.length >= 3) {
-      breadcrumb += " > ";
-      let name  = pathName[2];
-      switch (name) {
-        case "create-user":
-         breadcrumb += "Create New User"
-         break;
-        case "edit-user":
-          breadcrumb += "Edit User"
-          break;
-        case "create-asset":
-          breadcrumb += "Create New Asset"
-          break;
-        default:
-          breadcrumb += "";
-      }
-    }
-
+  }
 
   return (
     <div className="bg-primary h-14 flex justify-center text-lg font-semibold">
