ac345c5f75a2e0c4df69d52ab7a39264b1794ca6
Blue Teeth <daominhtri1000@gmail.com>
2024-07-09 15:04:50 +0700

    Add filter returning request API

--- AssetManagement.Application/Services/Interfaces/IReturningRequestService.cs
+++ AssetManagement.Application/Services/Interfaces/IReturningRequestService.cs
@@ -1,4 +1,9 @@
-﻿namespace AssetManagement.Application.Services.Interfaces {
+﻿using AssetManagement.Contracts.Dtos.PaginationDtos;
+using AssetManagement.Contracts.Dtos.ReturningRequestDtos.Requests;
+using AssetManagement.Contracts.Dtos.ReturningRequestDtos.Responses;
+
+namespace AssetManagement.Application.Services.Interfaces {
     public interface IReturningRequestService {
+        Task<PagingDto<FilterReturningResponse>> FilterReturningAsync(FilterReturningRequest filter);
     }
 }
---
+++ AssetManagement.Contracts/Dtos/ReturningRequestDtos/Requests/FilterReturningRequest.cs
@@ -0,0 +1,19 @@
+﻿using AssetManagement.Contracts.Enums;
+using AssetManagement.Domain.Enums;
+
+namespace AssetManagement.Contracts.Dtos.ReturningRequestDtos.Requests {
+    public class FilterReturningRequest {
+        public string? Search { get; set; }
+        public ReturningRequestState[]? States { get; set; }
+        public DateTime? ReturnedDate { get; set; }
+        public SortOption? SortAssetCode { get; set; }
+        public SortOption? SortAssetName { get; set; }
+        public SortOption? SortRequestedBy { get; set; }
+        public SortOption? SortAssignedDate { get; set; }
+        public SortOption? SortAcceptedBy { get; set; }
+        public SortOption? SortReturnedDate { get; set; }
+        public SortOption? SortState { get; set; }
+        public int? PageNumber { get; set; }
+        public int? PageSize { get; set; }
+    }
+}
