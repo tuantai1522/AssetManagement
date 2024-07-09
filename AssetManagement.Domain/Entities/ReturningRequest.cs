9b0de7340fdf09f3b32812d71d6db5d3f25a0713
Blue Teeth <daominhtri1000@gmail.com>
2024-07-09 13:06:49 +0700

    Update entity

--- AssetManagement.Domain/Entities/ReturningRequest.cs
+++ AssetManagement.Domain/Entities/ReturningRequest.cs
@@ -8,13 +8,13 @@ namespace AssetManagement.Domain.Entities {
 
         public ReturningRequestState State { get; set; }
 
-        public DateTime ReturnedDate { get; set; } = DateTime.Now;
+        public DateTime? ReturnedDate { get; set; }
 
         public virtual Assignment Assignment { get; set; } = null!;
         public Guid AssignmentId { get; set; }
 
-        public virtual AppUser AcceptedByUser { get; set; } = null!;
-        public Guid AcceptedByUserId { get; set; }
+        public virtual AppUser? AcceptedByUser { get; set; } = null!;
+        public Guid? AcceptedByUserId { get; set; }
 
     }
 }
