2ce5fac07b7ee5c3d5d105fa0a8a6268bd5674d5
Dat Bui <bvdat1110@outlook.com.vn>
2024-07-10 10:01:50 +0700

    Change the name and remove comments

--- AssetManagement.Application.Tests/Services/ReturningRequestTests/CompleteReturnRequestByIdAsyncTest.cs
+++ AssetManagement.Application.Tests/Services/ReturningRequestTests/CompleteReturnRequestByIdAsyncTest.cs
@@ -12,9 +12,9 @@ using System.Threading.Tasks;
 
 namespace AssetManagement.Application.Tests.Services.ReturningRequestTests
 {
-	public class CompleteReturnRequestByIdAsync : SetupReturningRequestServiceTest
+	public class CompleteReturnRequestByIdAsyncTest : SetupReturningRequestServiceTest
 	{
-        public CompleteReturnRequestByIdAsync() : base() { }
+        public CompleteReturnRequestByIdAsyncTest() : base() { }
 		[Fact]
 		public async Task CompleteReturnRequestByIdAsync_ReturningRequestNotFound_ThrowsNotFoundException()
 		{
@@ -73,8 +73,10 @@ namespace AssetManagement.Application.Tests.Services.ReturningRequestTests
 			_currentUserMock.Setup(c => c.UserId).Returns(userId);
 
 			_userManagerMock.Setup(x => x.Users).Returns(_users.AsQueryable().BuildMockDbSet().Object);
+			
+			var returningRequests = _returningRequests.ToList();
 
-			var request = _returningRequests.First(r => r.State == ReturningRequestState.WaitingForReturning);
+			var request = returningRequests.First(r => r.State == ReturningRequestState.WaitingForReturning);
 
 			_unitOfWorkMock.Setup(u => u.ReturningRequestRepository.GetRequestByIdAsync(It.IsAny<Guid>()))
 						   .ReturnsAsync(request);
@@ -83,10 +85,10 @@ namespace AssetManagement.Application.Tests.Services.ReturningRequestTests
 			await _returningRequestService.CompleteReturnRequestByIdAsync(request.Id);
 
 			// Assert
-			Assert.Equal(ReturningRequestState.Completed, request.State);
-			Assert.Equal(AssignmentState.Returned, request.Assignment.State);
-			Assert.Equal(AssetState.Available, request.Assignment.Asset!.State);
-			Assert.Equal(userId, request.AcceptedByUserId);
+			Assert.Equal(ReturningRequestState.Completed, returningRequests.First(r => r.Id == request.Id).State);
+			Assert.Equal(AssignmentState.Returned, returningRequests.First(r => r.Id == request.Id).Assignment.State);
+			Assert.Equal(AssetState.Available, returningRequests.First(r => r.Id == request.Id).Assignment.Asset!.State);
+			Assert.Equal(userId, returningRequests.First(r => r.Id == request.Id).AcceptedByUserId);
 			_unitOfWorkMock.Verify(u => u.SaveChangesAsync(), Times.Once);
 		}
 	}
